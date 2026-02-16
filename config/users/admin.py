75cb35771586db77c2d3afc1ab77049a6b52f347
ballepallirakesh <ballepallirakesh@gmail.com>
2026-02-16 11:22:01 +0530

    Day2: Added Company, Job, Application models with relationships

--- config/users/admin.py
+++ config/users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Userprofile
+from .models import Userprofile,Company,Job,Application
 admin.site.register(Userprofile)
+admin.site.register(Company)
+admin.site.register(Job)
+admin.site.register(Application)
 
 # Register your models here.
--- config/users/models.py
+++ config/users/models.py
@@ -9,3 +9,28 @@ class Userprofile(models.Model):
     
     def __str__(self):
         return self.name
+class Company(models.Model):
+    name=models.CharField(max_length=150)
+    location=models.CharField(max_length=150)
+    website=models.URLField(blank=True)
+    
+    def __str__(self):
+        return self.name
+class Job(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.TextField()
+    location=models.CharField(max_length=200)
+    salary=models.IntegerField()
+    company=models.ForeignKey(Company,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+class Application(models.Model):
+    user=models.ForeignKey(Userprofile,on_delete=models.CASCADE)
+    job=models.ForeignKey(Job,on_delete=models.CASCADE)
+    resume=models.TextField()
+    applied_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.user.name} + {self.job.title}"
